5c1ad87d0e6042c425af986b794d6a2934a861b2
Lin Zhang <myhere.2009@gmail.com>
2012-03-10 21:26:14 +0800

    add files

---
+++ double_click_gotop.js
@@ -0,0 +1,39 @@
+/**
+ * Author: linqian.zl@taobao.com
+ */
+define(function(require) {
+//LabsJS.define(function(require) {
+
+  var doc = document;
+
+  function bindDbClick() {
+    var S = window.KISSY;
+
+    if (S) {
+      S.Event.on(doc.body, 'dblclick', dbClickHandler)
+    } else {
+      $ = require('jquery-1.6.1');
+      $(doc.body).dblclick(dbClickHandler)
+    }
+  }
+
+  function dbClickHandler(evt) {
+    var target = evt.target;
+    if (target.tagName !== 'BODY') {
+      return;
+    }
+
+    try {
+      doc.body.scrollTop = 0;
+      doc.documentElement.scrollTop = 0;
+    } catch(e) {}
+  }
+
+  function init() {
+    bindDbClick();
+  }
+
+  init();
+});
+
+//@ sourceURL=double_click_gotop.js
---
+++ manifest.js
@@ -0,0 +1,9 @@
+TLabs.addFeature({
+	name: 'Ë«»÷»Ø¶¥²¿',
+	version: '1.0',
+	mods: [{
+		matches: '*.taobao.com|*.tmall.com',
+		path: 'double_click_gotop.js'
+	}]
+});
+
